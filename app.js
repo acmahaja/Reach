92034982fdab1e2d4caf253ccc6182e55c183554
acmahaja <acmahaja@gmail.com>
2021-04-03 21:32:17 +0200

    Added the ability for the user to create an account, login, create mood entry

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 const express = require('express');
+const expressValidator = require('express-validator')
 const session = require('express-session')
 const app = express();
+const bodyParser = require('body-parser');
+const router = express.Router()
+
+const { v4: uuidv4 } = require('uuid');
 
 const mongoose = require('mongoose')
 const path = require('path')
-const bodyParser = require('body-parser');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
+app.set('trust proxy', 1) // trust first proxy
+
+app.use(session({ secret: 'algorithms are fun', resave: false, saveUninitialized: false, cookie: { maxAge: Infinity } }));
+
+
+app.use(express.urlencoded());
+
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/model'));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-const UserModel = require('./model/UserModel');
-const Users = mongoose.model('Users', UserModel);
-
-mongoose.connect('mongodb://localhost:27017/AdressBook', {
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+mongoose.connect('mongodb://localhost:27017/HackMelbourne', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -25,6 +37,19 @@ mongoose.connect('mongodb://localhost:27017/AdressBook', {
     console.log(error);
 })
 
+
+app.use(express.static(__dirname + '../public'));
+app.use(express.static(__dirname + '../model'));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const UserModel = require('./model/UserModel');
+const Users = mongoose.model('Users', UserModel);
+
+const MoodModel = require('./model/MoodModel');
+const Moods = mongoose.model('Mood', MoodModel);
+
+const DiaryModel = require('./model/DiaryModel');
+const Diarys = mongoose.model('Diarys', DiaryModel);
 
 app.get('/user/:userID/diary/:entryID/edit', (req, res) => {
     const { userID, moodID } = req.params;
@@ -48,10 +73,25 @@ app.get('/user/:userID/diary', (req, res) => {
 })
 
 
-app.get('/user/:userID/mood/:moodID/edit', (req, res) => {
+app.get('/user/:userID/mood/:moodID/edit', async(req, res) => {
     const { userID, moodID } = req.params;
-    res.render('pages/user/mood/edit', req.params)
+    //res.render('pages/user/mood/moodEntry', req.params)
+
+    let userData = {};
+    await Users.findById(userID)
+        .then(result => {
+            Object.assign(userData, result._doc)
+        }).then(
+            await Moods.findById(moodID)
+            .then(result => {
+                Object.assign(userData, result._doc)
+                console.log(userData);
+                res.render('pages/user/mood/edit', userData)
+            })
+        )
 })
+
+
 
 app.get('/user/:userID/mood/new', (req, res) => {
     const { userID, moodID } = req.params;
@@ -59,15 +99,49 @@ app.get('/user/:userID/mood/new', (req, res) => {
 })
 
 
-app.get('/user/:userID/mood/:moodID', (req, res) => {
+app.get('/user/:userID/mood/:moodID', async(req, res) => {
     const { userID, moodID } = req.params;
-    res.render('pages/user/mood/moodEntry', req.params)
+    //res.render('pages/user/mood/moodEntry', req.params)
+
+    let userData = {};
+    await Users.findById(userID)
+        .then(result => {
+            Object.assign(userData, result._doc)
+        }).then(
+            await Moods.findById(moodID)
+            .then(result => {
+                Object.assign(userData, result._doc)
+                console.log(userData);
+                res.render('pages/user/mood/moodEntry', userData)
+            })
+        )
 })
 
-app.get('/user/:userID/mood', (req, res) => {
+app.post('/user/:userID/mood', (req, res) => {
     const { userID } = req.params;
     res.render('pages/user/mood/moodPage', req.params)
 })
+
+// $or: [userID]
+
+app.get('/user/:userID/mood', async(req, res) => {
+    const userID = {};
+    userID.userID = req.params.userID;
+
+    let userData = {};
+
+    await Users.findById(req.params.userID)
+        .then(result => Object.assign(userData, result._doc)).then(
+            Moods.find({ $or: [userID] }, function(err, moods) {
+                userData.moods = moods
+                    //res.send(userData);
+                res.render('pages/user/mood/moodPage', userData);
+            })
+        );
+
+
+});
+
 
 
 app.get('/user/:userID/helpcall', (req, res) => {
@@ -86,16 +160,27 @@ app.get('/user/:userID/resources', (req, res) => {
     res.render('pages/user/resources', req.params)
 })
 
-
-app.get('/user/:userID', (req, res) => {
-    //const { "userID": userID } = req.params;
-    console.log(req.params);
-    res.render('pages/user/userpage', req.params)
+app.get('/user/:userID', async(req, res) => {
+    await Users.findById(req.params.userID)
+        .then(result => res.render('pages/user/userpage', result));
+    // res.render('pages/user/userpage', result)
 })
 
+app.post('/user/new', async(req, res) => {
 
-app.get('/helpcall', (req, res) => {
-    res.render('pages/help');
+    const requestBody = req.body;
+    requestBody._id = uuidv4();
+    const d = new Date();
+    console.log(requestBody);
+    const newUser = new Users(requestBody);
+    await newUser.save().then((res.redirect(`/user/${requestBody._id}`))).catch((error) => {
+        const userID = {};
+        userID._id = req.params.userID;
+        const requestBody = {};
+        requestBody.email = req.body.email;
+
+        console.log(error);
+    });
 })
 
 
@@ -103,6 +188,29 @@ app.get('/signup', (req, res) => {
     res.render('pages/signup');
 })
 
+app.get('/helpcall', (req, res) => {
+    res.render('pages/help');
+})
+
+app.post('/login', async(req, res) => {
+    const userID = {};
+    userID._id = req.params.userID;
+    const requestBody = {};
+    requestBody.email = req.body.email;
+    await Users.findOne(requestBody).then(result => {
+        console.log(result.password)
+        console.log(req.body.password)
+        if (result.password === req.body.password) {
+            console.log(result);
+            res.redirect(`/user/${result._id}`)
+        }
+        res.redirect('/login')
+    }).catch(err => {
+        console.log(err);
+        res.redirect('/login')
+    })
+
+})
 
 app.get('/login', (req, res) => {
     res.render('pages/login');
@@ -120,6 +228,13 @@ app.get('/about', (req, res) => {
 
 
 app.get('/', (req, res) => {
+    const sess = req.session
+    console.log(sess);
+    if (sess.userid) {
+        res.status = 200;
+        res.redirect(`/user/${sess.userid}/resources`);
+    }
+    console.log(sess.userid);
     res.render('pages/index');
 })
 
--- scripts/populateDB.js
+++ scripts/populateDB.js
@@ -19,7 +19,7 @@ const Moods = mongoose.model('Mood', MoodModel);
 const DiaryModel = require('../model/DiaryModel');
 const Diarys = mongoose.model('Diarys', DiaryModel);
 
-mongoose.connect('mongodb://localhost:27017/UserBook', {
+mongoose.connect('mongodb://localhost:27017/HackMelbourne', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
@@ -28,58 +28,58 @@ mongoose.connect('mongodb://localhost:27017/UserBook', {
     console.log(error);
 });
 
-app.post('/newUser', async(req, res) => {
-    const uuidVal = uuidv4();
-    const d = new Date();
-    const newUsers = new Users({
-        _id: uuidVal,
-        name: "acmahaja",
-        email: "acmahaja@email.com",
-        password: "password",
-        firstname: "A",
-        lastname: "J",
-        place: "place",
-    });
+// app.post('/newUser', async(req, res) => {
+const uuidVal = "691237b6-f6f2-4a27-891f-5438807b75a3";
+const d = new Date();
+// const newUsers = new Users({
+//     _id: uuidVal,
+//     email: "acmahaja@email.com",
+//     password: "password",
+//     firstname: "A",
+//     lastname: "J",
+// });
 
-    const newMoods = new Moods({
-        _id: uuidv4(),
-        userID: uuidVal,
-        date: d.toString(),
-        moods: { "happy": 1, "sad": 0 },
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae unde tenetur harum! Possimus ratione, ipsum sed qui incidunt inventore quae quis veritatis debitis totam quaerat perferendis repudiandae facere maxime."
-    });
+const newMoods = new Moods({
+    _id: uuidv4(),
+    userID: uuidVal,
+    date: d.toString(),
+    moods: { "happy": 1, "sad": 0 },
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae unde tenetur harum! Possimus ratione, ipsum sed qui incidunt inventore quae quis veritatis debitis totam quaerat perferendis repudiandae facere maxime."
+});
 
-    const newDiarys = new Diarys({
-        _id: uuidv4(),
-        userID: uuidVal,
-        date: d.toString(),
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae unde tenetur harum! Possimus ratione, ipsum sed qui incidunt inventore quae quis veritatis debitis totam quaerat perferendis repudiandae facere maxime."
+const newDiarys = new Diarys({
+    _id: uuidv4(),
+    userID: uuidVal,
+    date: d.toString(),
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quae unde tenetur harum! Possimus ratione, ipsum sed qui incidunt inventore quae quis veritatis debitis totam quaerat perferendis repudiandae facere maxime."
 
-    });
-    let dbMessage = ""
-    await newUsers.save().then((user) => {
-        dbMessage += user
-    }).catch((error) => {
-        console.log(error);
-    });
+});
+let dbMessage = ""
+    // newUsers.save().then((user) => {
+    //     dbMessage += user
+    // }).catch((error) => {
+    //     console.log(error);
+    // });
 
-    await newMoods.save().then((mood) => {
-        dbMessage += mood
-    }).catch((error) => {
-        console.log(error);
-    });
+newMoods.save().then((mood) => {
+    dbMessage += mood
+}).catch((error) => {
+    console.log(error);
+});
 
-    await newDiarys.save().then((diary) => {
-        dbMessage += diary
-        res.send(dbMessage);
-    }).catch((error) => {
-        console.log(error);
-    });
-
-    res.send(dbMessage);
+newDiarys.save().then((diary) => {
+    dbMessage += diary
+    console.log(dbMessage);
+    // res.send(dbMessage);
+}).catch((error) => {
+    console.log(error);
 });
 
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Listening on port 3000");
-});
+// res.send(dbMessage);
+// });
+
+
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log("Listening on port 3000");
+// });
