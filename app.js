1d343d653614ca242f4ddc765f4490eac70d41ae
acmahaja <acmahaja@gmail.com>
2021-04-03 14:51:15 +0200

    Added node modules for cookies

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const session = require('express-session')
 const app = express();
+
 const mongoose = require('mongoose')
 const path = require('path')
 const bodyParser = require('body-parser');
